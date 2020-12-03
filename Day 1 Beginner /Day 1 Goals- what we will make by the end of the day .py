2a823d826a230c0a84d26b911fa9229b3dcad49c
AingHongSin <67510814+AingHongSin@users.noreply.github.com>
2020-12-03 22:21:04 +0700

    Day1

---
+++ Day 1 Beginner /Day 1 Goals- what we will make by the end of the day .py
@@ -0,0 +1,4 @@
+print("The Band Generator")
+birthCityName = input("Enter your bithd city: ")
+PetName = input("Enter your pet Name: ")
+print("Your band should be put name: ", birthCityName + PetName)
